52be621838bfd7b67ae4708299000a1c7381f77a
meshack254 <meshackmutinda555@gmail.com>
2023-07-08 12:22:06 +0300

    Fixed type errors in sanity.config.ts and env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -6,18 +6,18 @@ function assertValue<T>(v: T | null, errorMessage: string): T {
   return v;
 }
 
-const projectId = assertValue(
+const projectId:string = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
-);
+) as string;
 
 const apiVersion
 = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-12';
 
-const dataset = assertValue(
+const dataset: string = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
-);
+) as string;
 
 const useCdn = false;
 
